2b930e686b4bc460daa74f2e112bad0b01426761
Abdullah Bajwa <a_bajwa01@outlook.com>
2025-03-26 21:31:51 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 module.exports = {
   tcpPort: process.env.TCP_PORT || 3000,
-  loginUrl: "https://trackerbe.onrender.com/auth/adminLogin",
-  apiUrl: "https://trackerbe.onrender.com",
+  loginUrl: "https://trackerbe.avantlabstech.com.com/auth/adminLogin",
+  apiUrl: "https://trackerbe.avantlabstech.com",
   email: process.env.ADMIN_EMAIL,
   password: process.env.PASSWORD,
 };
