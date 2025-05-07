40ba2e4a5ef0cd3f3b64f528bc4fefdfd4650cf5
Abdullah <a_bajwa01@outlook.com>
2025-05-07 20:55:37 +0500

    fix: correct duplicate domain in loginUrl

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   tcpPort: process.env.TCP_PORT || 3000,
-  loginUrl: "https://trackerbe.avantlabstech.com.com/auth/adminLogin",
+  loginUrl: "https://trackerbe.avantlabstech.com/auth/adminLogin",
   apiUrl: "https://trackerbe.avantlabstech.com",
   email: process.env.ADMIN_EMAIL,
   password: process.env.PASSWORD,
